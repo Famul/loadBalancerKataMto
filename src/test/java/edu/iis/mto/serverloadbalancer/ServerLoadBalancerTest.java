82fd2272689b20e5c753ff3c0ac8ab2a4b94e27c
Famulski.Michal <187684@edu.p.lodz.pl>
2018-04-10 18:22:43 +0200

    balance serwer with 0 vms implemented

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -12,5 +13,47 @@ public class ServerLoadBalancerTest {
 		assertThat(true, equalTo(true));
 	}
 
+	@Test
+	public void loadBalanceOfServerWithoutVMsIsEmpty() {
+	    Serwer theSerwer = a(serwer().withCapacity(1));
+	    
+	    balance(aListOfServersWith(theSerwer), anEmptyListOfVms());
+	    assertThat(theSerwer, hasCorrectPercentageOf(0.0d));
+	}
+    private Matcher<? super Serwer> hasCorrectPercentageOf(double expectedLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+    }
 
+    private void balance(Serwer[] serwers, VM[] vms) {
+        new ServerLoadBalancer().balance(serwers, vms);
+    }
+
+    private VM[] anEmptyListOfVms() {
+        return new VM[0];
+    }
+
+    private Serwer[] aListOfServersWith(Serwer... Serwers) {
+        return Serwers;
+    }
+
+    private Serwer a(ServerBuilder serverBuilder) {
+        
+        return serverBuilder.build();
+    }
+
+    private ServerBuilder serwer() {
+        // TODO Auto-generated method stub
+        return new ServerBuilder();
+    }
+
+	
 }
+/*
+ctrl + .                -- do najblizszego bledu
+ctrl + 1                -- mozliwe rozwiazania bledu
+ctrl + F6               -- przelaczanie okien
+ctrl + d                -- usun linie
+alt + shift + r         -- rename
+ctrl + shift + l        -- list of hotkeys    
+alt + shift + x  > t    -- start test
+*/
