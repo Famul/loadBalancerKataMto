e7f29a6a4e4c50241d42cb2a80414e0f0e97f38b
Famulski.Michal <187684@edu.p.lodz.pl>
2018-05-30 18:45:26 +0200

    test5 refactorization

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,15 +4,24 @@ public class ServerLoadBalancer {
 
     public void balance(Server[] servers, Vm[] vms) {
         for (Vm vm : vms) {
-            Server lessLoadedServer = null;
-            for (Server server : servers) {
-                if (lessLoadedServer == null || lessLoadedServer.currentLoadPercentage > server.currentLoadPercentage) {
-                    lessLoadedServer = server;
-                }
-            }
-            lessLoadedServer.addVm(vm);
+            addVmToLessLoadedServer(servers, vm);
 
         }
+    }
+
+    private void addVmToLessLoadedServer(Server[] servers, Vm vm) {
+        Server lessLoadedServer = findLessLoadedServer(servers);
+        lessLoadedServer.addVm(vm);
+    }
+
+    private Server findLessLoadedServer(Server[] servers) {
+        Server lessLoadedServer = null;
+        for (Server server : servers) {
+            if (lessLoadedServer == null || lessLoadedServer.currentLoadPercentage > server.currentLoadPercentage) {
+                lessLoadedServer = server;
+            }
+        }
+        return lessLoadedServer;
     }
 
 }
