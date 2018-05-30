a4fdd0c876508d493ef44323338ff93523ac0a27
Famulski.Michal <187684@edu.p.lodz.pl>
2018-05-30 15:08:09 +0200

    balancing server with no vms

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,16 +1,48 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
 
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
 
+    @Test
+    public void serverWithOneSlotAndZeroVmsShouldStayEmpty() {
+        Server theServer = a(server().withCapacity(1));
+
+        balancing(aListOfServersWith(theServer), anEmptyListOfVms());
+        Assert.assertThat(theServer, hasALoadPercentageOf(0.0d));
+    }
+
+    private Matcher<? super Server> hasALoadPercentageOf(double expectedLoadPercentage) {
+        return new loadPercentageMatcher(expectedLoadPercentage);
+    }
+
+    private void balancing(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers, vms);
+    }
+
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
+
+    private Server a(ServerBuilder builder) {
+        return builder.build();
+    }
+
+    private Server[] aListOfServersWith(Server... servers) {
+        return servers;
+    }
+
+    private ServerBuilder server() {
+        return new ServerBuilder();
+    }
 }
