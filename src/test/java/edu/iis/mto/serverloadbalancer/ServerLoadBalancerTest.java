a8b6163632c780ad141408dcd3f1cc264a6fb7d2
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-12 14:01:45 +0200

    server with vm of size equal to server capacity should be full

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -23,6 +23,29 @@ public class ServerLoadBalancerTest {
         assertThat(theServer, serverWithCurrentLoadPercentageOf(0.0d));
     }
 
+    @Test
+    public void serverWithVmEqualToCapacityShouldBeFull() {
+        Server theServer = a(server().withCapacityOf(1));
+        Vm theVm = a(vm().ofSize(1));
+        balancing(aServerListWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat(theServer, serverWithCurrentLoadPercentageOf(100.0d));
+        assertThat("the server should contain the vm", theServer.contains(theVm));
+
+    }
+
+    private Vm a(VmBuilder builder) {
+        return builder.build();
+    }
+
+    private Vm[] aListOfVmsWith(Vm... vms) {
+        return vms;
+    }
+
+    private VmBuilder vm() {
+        return new VmBuilder();
+    }
+
     private void balancing(Server[] servers, Vm[] vms) {
         new ServerLoadBalancer().balance(servers, vms);
     }
