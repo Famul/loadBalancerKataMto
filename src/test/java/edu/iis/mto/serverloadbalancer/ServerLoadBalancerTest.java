b5ef9c77d6de4ad6296c59ba448ef773683ca01a
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-12 13:54:51 +0200

    server with no vms stays empty

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,16 +1,49 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
 
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
+
+    @Test
+    public void serverWithNoVmsShouldBeEmpty() {
+        Server theServer = a(server().withCapacityOf(1));
+
+        balancing(aServerListWith(theServer), anEmptyListOfVms());
+
+        assertThat(theServer, serverWithCurrentLoadPercentageOf(0.0d));
+    }
+
+    private Matcher<? super Server> serverWithCurrentLoadPercentageOf(double expectedLoadPercentage) {
+        return new serverLoadPercentageMatcher(expectedLoadPercentage);
+    }
+
+    private void balancing(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers, vms);
+    }
+
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
+
+    private Server a(ServerBuilder builder) {
+        return builder.build();
+    }
+
+    private Server[] aServerListWith(Server... servers) {
+        return servers;
+    }
+
+    private ServerBuilder server() {
+        return new ServerBuilder();
+    }
 
 }
---
+++ src/test/java/edu/iis/mto/serverloadbalancer/serverLoadPercentageMatcher.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class serverLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+
+    private double expectedLoadPercentage;
+
+    public serverLoadPercentageMatcher(double expectedLoadPercentage) {
+        this.expectedLoadPercentage = expectedLoadPercentage;
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("server with current load percentage of ").appendValue(expectedLoadPercentage);
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description description) {
+        description.appendText("server with current load percentage of ").appendValue(item.currentLoadPercentage);
+    }
+
+    @Override
+    protected boolean matchesSafely(Server server) {
+        return server.currentLoadPercentage == expectedLoadPercentage ||
+                Math.abs(expectedLoadPercentage - server.currentLoadPercentage) < 0.01d;
+    }
+
+}
