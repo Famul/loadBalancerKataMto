bc3d48f74a601a9528ede16d1564c44aa5cdd30a
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-12 14:11:38 +0200

    test3 refactorization

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -2,6 +2,7 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
 
+    private static final double MAX_LOAD = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
 
@@ -12,6 +13,10 @@ public class Server {
 
     public boolean contains(Vm theVm) {
         return true;
+    }
+
+    public void addVm(Vm vm) {
+        currentLoadPercentage = (double) vm.size / (double) capacity * MAX_LOAD;
     }
 
 }
