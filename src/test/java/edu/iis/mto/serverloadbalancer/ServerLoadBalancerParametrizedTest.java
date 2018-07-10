43861a9c5c04c9fe5106325ae2705b04a9e7fa24
Famulski.Michal <187684@edu.p.lodz.pl>
2018-07-10 17:12:39 +0200

    Added Parameterization to first test

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -5,18 +5,38 @@ import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
-	@Test
-	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+@RunWith(Parameterized.class)
+public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest {
 
-		assertThat(theServer, hasLoadPercentageOf(100.0d));
-		assertThat("the server should contain vm", theServer.contains(theVm));
-	}
-	
-	
+    private int serverCapacity;
+    private int vmSize;
+
+    @Parameters
+    public static Collection<Object[]> testData() {
+        return Arrays.asList(new Object[][] {{2, 2}, {5, 5}, {8, 8}, {14, 14}, {25, 25}});
+    }
+
+    public ServerLoadBalancerParametrizedTest(int serverCapacity, int vmSize) {
+        this.serverCapacity = serverCapacity;
+        this.vmSize = vmSize;
+    }
+
+    @Test
+    public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
+        Server theServer = a(server().withCapacity(serverCapacity));
+        Vm theVm = a(vm().ofSize(vmSize));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat(theServer, hasLoadPercentageOf(100.0d));
+        assertThat("the server should contain vm", theServer.contains(theVm));
+    }
+
 }
