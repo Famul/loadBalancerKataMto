00e12b60869b6f29e5b7160fafd3ada223c8a4ae
Famulski.Michal <187684@edu.p.lodz.pl>
2018-07-10 18:43:11 +0200

    Added 7th test as parameterized test

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -5,35 +5,34 @@ import java.util.List;
 
 public class Server {
 
-	public static final double MAXIMUM_LOAD = 100.0d;
-	public double currentLoadPecentage;
-	private int capacity;
-	private List<Vm> vms = new ArrayList<Vm>();
+    public static final double MAXIMUM_LOAD = 100.0d;
+    public double currentLoadPecentage;
+    private int capacity;
+    private List<Vm> vms = new ArrayList<Vm>();
 
-	public Server(int capacity) {
-		this.capacity = capacity;
-	}
+    public Server(int capacity) {
+        this.capacity = capacity;
+    }
 
-	public boolean contains(Vm vm) {
-		return vms.contains(vm);
-	}
+    public boolean contains(Vm vm) {
+        return vms.contains(vm);
+    }
 
-	public void addVm(Vm vm) {
-		this.vms.add(vm);
-		this.currentLoadPecentage = (double) vm.size / (double) this.capacity
-				* MAXIMUM_LOAD;
-	}
+    public void addVm(Vm vm) {
+        this.vms.add(vm);
+        this.currentLoadPecentage += (double) vm.size / (double) this.capacity * MAXIMUM_LOAD;
+    }
 
-	public int vmsCount() {
-		return vms.size();
-	}
+    public int vmsCount() {
+        return vms.size();
+    }
 
-	public int getCapacity() {
-		return capacity;
-	}
+    public int getCapacity() {
+        return capacity;
+    }
 
-	public boolean canFit(Vm vm) {		
-		return currentLoadPecentage + ((double)vm.size / (double)this.capacity *MAXIMUM_LOAD) <= MAXIMUM_LOAD ;
-	}
+    public boolean canFit(Vm vm) {
+        return currentLoadPecentage + ((double) vm.size / (double) this.capacity * MAXIMUM_LOAD) <= MAXIMUM_LOAD;
+    }
 
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -18,15 +18,52 @@ public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTe
 
     private int serverCapacity;
     private int vmSize;
+    private int numberOfServers;
+    private int numberOfVms;
+    private int[] capacities;
+    private int[] sizes;
+    private double[] loadPercentages;
+
+    /*
+     * @Parameters public static Collection<Object[]> testData() { return Arrays.asList(new Object[][] {{2, 2, 2, 3, {6,
+     * 10}, {}, {}}, {5, 5}, {8, 8}, {14, 14}, {25, 25}}); }
+     * 
+     */
 
     @Parameters
-    public static Collection<Object[]> testData() {
-        return Arrays.asList(new Object[][] {{2, 2}, {5, 5}, {8, 8}, {14, 14}, {25, 25}});
+    public static Collection<Object[][]> testData() {
+        return Arrays.asList(new Object[][][] {{{2}, {2}, {2}, {3}, {6, 10}, {3, 1, 5}, {50.00, 60.00}},
+                {{5}, {5}, {3}, {4}, {9, 4, 8}, {1, 2, 3, 5}, {66.66, 50.00, 37.50}},
+                {{10}, {10}, {2}, {4}, {15, 24}, {3, 4, 2, 5}, {53.33, 25.00}},
+                {{23}, {23}, {4}, {3}, {14, 4, 10, 2}, {7, 9, 1}, {50.00, 25.00, 90.00, 0.00}},
+                {{1}, {1}, {3}, {4}, {10, 4, 6}, {1, 6, 4, 8}, {90.00, 100.00, 100.00}}});
     }
 
-    public ServerLoadBalancerParametrizedTest(int serverCapacity, int vmSize) {
-        this.serverCapacity = serverCapacity;
-        this.vmSize = vmSize;
+    public ServerLoadBalancerParametrizedTest(Object[] serverCapacity, Object[] vmSize, Object[] numberOfServers,
+            Object[] numberOfVms, Object[] capacities, Object[] sizes, Object[] loadPercentages) {
+        this.serverCapacity = ((Integer) serverCapacity[0]).intValue();
+        this.vmSize = ((Integer) vmSize[0]).intValue();
+        this.numberOfServers = ((Integer) numberOfServers[0]).intValue();
+        this.numberOfVms = ((Integer) numberOfVms[0]).intValue();
+        this.capacities = castObjectArrayToIntArray(capacities);
+        this.sizes = castObjectArrayToIntArray(sizes);
+        this.loadPercentages = castObjectArrayToDoubleArray(loadPercentages);
+    }
+
+    public int[] castObjectArrayToIntArray(Object[] object) {
+        int[] array = new int[object.length];
+        for (int i = 0; i < object.length; i++) {
+            array[i] = ((Integer) object[i]).intValue();
+        }
+        return array;
+    }
+
+    public double[] castObjectArrayToDoubleArray(Object[] object) {
+        double[] array = new double[object.length];
+        for (int i = 0; i < object.length; i++) {
+            array[i] = ((Double) object[i]).doubleValue();
+        }
+        return array;
     }
 
     @Test
@@ -39,4 +76,24 @@ public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTe
         assertThat("the server should contain vm", theServer.contains(theVm));
     }
 
+    @Test
+    public void balancingMultipleServersWithMultipleVms() {
+        Server[] listOfServers = new Server[numberOfServers];
+        for (int i = 0; i < numberOfServers; i++) {
+            listOfServers[i] = a(server().withCapacity(capacities[i]));
+        }
+        Vm[] listOfVms = new Vm[numberOfVms];
+        for (int i = 0; i < numberOfVms; i++) {
+            listOfVms[i] = a(vm().ofSize(sizes[i]));
+        }
+
+        balance(listOfServers, listOfVms);
+
+        // assertThat("the first server should contain the first vm", theFirstServer.contains(theFirstVm));
+        // assertThat("the second server should contain the second vm", theSecondServer.contains(theSecondVm));
+        // assertThat("the first server should contain the third vm", theFirstServer.contains(theThirdVm));
+        for (int i = 0; i < numberOfServers; i++) {
+            assertThat(listOfServers[i], hasLoadPercentageOf(loadPercentages[i]));
+        }
+    }
 }
